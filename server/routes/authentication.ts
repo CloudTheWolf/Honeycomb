2617a176cc38162a9f9a014c1abed5290ee27840
CloudTheWolf <ukkmmichael@gmail.com>
2025-06-28 15:54:37 +0100

    Improve Login Backend

--- server/routes/authentication.ts
+++ server/routes/authentication.ts
@@ -1,11 +1,13 @@
 import {Hono} from "hono";
 import {db} from "@server/helpers/database-helper";
 import {sessions, users} from "@server/db/schema";
-import {eq, or} from "drizzle-orm";
+import {eq, or,gt} from "drizzle-orm";
 import bcrypt from "bcryptjs";
 import {nanoid} from "nanoid";
-import {signJwt} from "@server/helpers/jwt-helper";
+import {signJwt, verifyJwt} from "@server/helpers/jwt-helper";
 import {deleteCookie, getCookie, setCookie} from 'hono/cookie'
+import {requireMinimumRole, requireRole} from "@server/middleware/requireRole";
+import {UserRole} from "@server/enums/UserRole";
 
 function getDomain(c: any): string | undefined {
     const host = c.req.header('host') ?? 'localhost'
@@ -15,48 +17,68 @@ function getDomain(c: any): string | undefined {
 
 const authenticationRouter = new Hono();
 
+authenticationRouter.get('/status', async (c) => {
+    const sessionToken = getCookie(c,'session')
+    if (!sessionToken) return c.json({ loggedIn: false })
+    try {
+        const now = new Date()
+
+        const session = await db.query.sessions.findFirst({
+            where: (s) =>
+                eq(s.accessToken, sessionToken) &&
+                eq(s.isRevoked, 0) &&
+                gt(s.expiresAt, now)
+        })
+        const payload = await verifyJwt(sessionToken)
+        return c.json({loggedIn: !!session,payload})
+    } catch (error) {
+        return c.json({ loggedIn: false })
+    }
+})
+
 authenticationRouter.post('/login', async (c) => {
-    const { username, password } = await c.req.json()
-    console.log(`${username}, ${password}  `)
-    const user = await db.query.users.findFirst({
-        where: or(eq(users.username,username),eq(users.email,username)),
-    })
+    try {
+        const {username, password} = await c.req.json()
+        console.log(`${username}, ${password}  `)
+        const user = await db.query.users.findFirst({
+            where: or(eq(users.username, username), eq(users.email, username)),
+        })
 
-    if(!user)
-    {
-        return c.json({ error: 'Invalid credentials' }, 401)
+        if (!user) {
+            return c.json({error: 'Invalid credentials'}, 401)
+        }
+
+        const match = bcrypt.compare(password, user.password)
+        if (!match) {
+            return c.json({error: 'Invalid credentials'}, 401)
+        }
+
+        const accessToken = await signJwt({userId: user.id, apiAccess: true, role: user.role})
+        const refreshToken = nanoid()
+        const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000)
+
+        await db.insert(sessions).values({
+            userId: Number(user.id),
+            accessToken,
+            refreshToken,
+            isRevoked: 0,
+            expiresAt
+        })
+
+        setCookie(c, 'session', accessToken, {
+            path: '/',
+            httpOnly: true,
+            secure: true,
+            maxAge: 24 * 60 * 60,
+            sameSite: 'Strict',
+            domain: getDomain(c),
+            expires: expiresAt,
+        })
+
+        return c.json({success: true})
+    } catch (error) {
+        return c.json({success: false, error: error}, 500)
     }
-
-    const match = bcrypt.compare(password, user.password)
-    if (!match) {
-        return c.json({ error: 'Invalid credentials' }, 401)
-    }
-
-    const accessToken = await signJwt({ userId: user.id, apiAccess: true })
-    const refreshToken = nanoid()
-    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-
-    await db.insert(sessions).values({
-        userId: Number(user.id),
-        accessToken,
-        refreshToken,
-        isRevoked: 0,
-        expiresAt
-    })
-
-    setCookie(c, 'session', accessToken, {
-        path: '/',
-        httpOnly: true,
-        secure: true,
-        maxAge: 7 * 24 * 60 * 60,
-        sameSite: 'Strict',
-        domain: getDomain(c),
-        expires: expiresAt,
-    })
-
-    return c.json({ success: true })
-
-
 })
 
 export default authenticationRouter;
